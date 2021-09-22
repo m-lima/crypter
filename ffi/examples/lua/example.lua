28258a39ae455b46fe320f967afb9b6fbf1eebbb
Marcelo Lima <marcelowind@gmail.com>
2021-09-22 19:31:11 +0200

    Make lua example more memory safe

--- ffi/examples/lua/example.lua
+++ ffi/examples/lua/example.lua
@@ -6,14 +6,18 @@ ffi.cdef[[
 
   RustSlice crypter_encrypt(struct Slice pass, struct Slice payload);
   RustSlice crypter_decrypt(struct Slice pass, struct Slice payload);
+  void crypter_free_slice(struct RustSlice slice);
 ]]
 
 local function slice_from_str(text)
   local slice = ffi.new('Slice')
+  local ptr = ffi.new('uint8_t[?]', #text)
 
-  slice.ptr = ffi.cast('uint8_t *', text)
-  slice.len = string.len(text)
-  return slice
+  ffi.copy(ptr, text, #text)
+
+  slice.ptr = ffi.cast('uint8_t *', ptr)
+  slice.len = #text
+  return slice, ptr
 end
 
 local function relax_rust_slice(rust_slice)
@@ -27,8 +31,9 @@ end
 -- Adapt this to your OS dynamic library format
 crypter = ffi.load('../../../target/release/libcrypter.dylib')
 
-local pass = slice_from_str('supersecret')
-local encrypted = crypter.crypter_encrypt(pass, slice_from_str('mega ultra safe payload'))
+local pass, pass_ptr = slice_from_str('supersecret')
+local payload, payload_ptr = slice_from_str('mega ultra safe payload')
+local encrypted = crypter.crypter_encrypt(pass, payload)
 local decrypted = crypter.crypter_decrypt(pass, relax_rust_slice(encrypted))
 
 if decrypted.ptr ~= nil then
@@ -36,3 +41,6 @@ if decrypted.ptr ~= nil then
 else
   print('Failed roud trip')
 end
+
+crypter.crypter_free_slice(encrypted)
+crypter.crypter_free_slice(decrypted)
